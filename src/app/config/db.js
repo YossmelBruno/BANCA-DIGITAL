475a4b67b0b5e41b9eb2ad2dbc46c7b01b8b601e
Yossmel Bruno <yossmelbrunoamado@gmail.com>
2026-04-29 05:09:11 -0500

    Fin del proyecto

--- src/app/config/db.js
+++ src/app/config/db.js
@@ -1,24 +1,18 @@
-import "dotenv/config";
 import mysql from "mysql2";
 
-console.log("MYSQLHOST:", process.env.MYSQLHOST);
-console.log("MYSQLUSER:", process.env.MYSQLUSER);
-console.log("MYSQLDATABASE:", process.env.MYSQLDATABASE);
-console.log("MYSQLPORT:", process.env.MYSQLPORT);
-
 const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: Number(process.env.MYSQLPORT)
+  host: "switchback.proxy.rlwy.net",
+  user: "root",
+  password: "NwSVwkyvekULDShLJkBWVXvoWOKUWxxk",
+  database: "banca-digital",
+  port: 53129
 });
 
 db.connect((err) => {
   if (err) {
     console.error("Error MySQL:", err);
   } else {
-    console.log("Conectado a Railway MySQL");
+    console.log("Conectado MySQL Railway");
   }
 });
 
